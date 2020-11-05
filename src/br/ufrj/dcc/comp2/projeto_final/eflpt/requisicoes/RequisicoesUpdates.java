f13016454424dd42cf57af502e05488f9783b057
Thiago Castro <thiago.castro99@hotmail.com>
2020-11-05 15:45:53 -0300

    Primeira versão da classe RequisicoesUpdates. Essa classe realiza requisições à API de um período até o dia atual.

---
+++ src/br/ufrj/dcc/comp2/projeto_final/eflpt/requisicoes/RequisicoesUpdates.java
@@ -0,0 +1,118 @@
+package br.ufrj.dcc.comp2.projeto_final.eflpt.requisicoes;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import br.ufrj.dcc.comp2.projeto_final.eflpt.Medicao;
+import br.ufrj.dcc.comp2.projeto_final.eflpt.Pais;
+import br.ufrj.dcc.comp2.projeto_final.eflpt.StatusCaso;
+import br.ufrj.dcc.comp2.projeto_final.eflpt.estatisticas.Dados;
+
+import java.io.IOException;
+import java.net.*;
+import java.net.http.*;
+import java.net.http.HttpClient.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class RequisicoesUpdates 
+{
+	public ArrayList<Medicao> realizaOperacoesAtualizacao(String tipo, String slug, String ultimaData, Dados d, StatusCaso status, Pais pais)
+	{
+			
+	   ArrayList<Medicao> resultados = new ArrayList<Medicao>();
+	   ArrayList<Medicao> tipoDados;
+	   
+	   if (status == StatusCaso.CONFIRMADOS)
+		   tipoDados = d.getConfirmados();
+	   
+	   else if (status == StatusCaso.MORTOS)
+		   tipoDados = d.getMortos();
+	   
+	   else
+		   tipoDados = d.getRecuperados();
+	   
+	   String dataAtual = LocalDateTime.now().toString();
+	   
+	   HttpClient cliente = HttpClient.newBuilder()
+               .version(Version.HTTP_2)
+               .followRedirects(Redirect.ALWAYS)
+               .build();
+
+	   HttpRequest requisicao = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.covid19api.com/total/country/" + slug + "/status/" + tipo + "?from=" + ultimaData + "&to=" + dataAtual))
+                    .build();
+	   
+	   try 
+	   {
+			
+		    HttpResponse<String> resposta = cliente.send(requisicao, HttpResponse.BodyHandlers.ofString());
+			
+		    int codStatus = resposta.statusCode();
+		    
+		    if (codStatus >= 200 && codStatus < 300)
+		    {
+			    try 
+			    {
+			        JSONArray casosPeriodo = (JSONArray) new JSONParser().parse(resposta.body());
+			        
+			        Object linha;
+			        LocalDateTime momento;
+			        long casos;
+			        
+			        Iterator<JSONArray> iterador = casosPeriodo.iterator();
+			        
+			        if (iterador.hasNext())
+			        {
+			        	linha = iterador.next();
+			        	momento = LocalDateTime.parse(((String) ((JSONObject) linha).get("Date")).replace("Z", ""));
+			        	casos = Long.parseLong(String.valueOf(( ((JSONObject) linha).get("Cases"))));
+			        	
+			        	if (casos != 0)
+			        		tipoDados.add(new Medicao(pais, momento, (int) casos, status));
+			        	System.out.println("Atualização:" + pais.getNome() + "\t" + casos);    	
+			        
+				        
+			        }
+			    } 
+				
+			    catch (ParseException e) {
+				
+			        System.err.println("Resposta inválida");
+				
+			        e.printStackTrace();
+			        return null;
+				
+			    }
+		    }
+		    else
+		    	System.out.println(codStatus);
+			
+		} 
+			
+		catch (IOException e) {
+			
+		    System.err.println("Problema com a conexão");
+			
+		    e.printStackTrace();
+		    return null;
+			
+		} 
+			
+		catch (InterruptedException e) {
+			
+		    System.err.println("Requisição interrompida");
+			
+		    e.printStackTrace();
+		    return null;
+			
+		}
+	   
+	   return resultados;
+
+	}
+
+}
